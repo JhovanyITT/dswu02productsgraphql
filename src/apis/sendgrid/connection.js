580ffe48bd30dfa95998cade3927d63df20414a8
Jaime Torres Reyes <jacatorresre@ittepic.edu.mx>
2024-11-15 20:31:18 -0700

    Agregando mi API_KEY Torres a connection.js

--- src/apis/sendgrid/connection.js
+++ src/apis/sendgrid/connection.js
@@ -1,6 +1,8 @@
 const sgMail = require('@sendgrid/mail');
+const sgMailTorres = require('@sendgrid/mail);
 
 // Configura tu API Key de SendGrid 
 sgMail.setApiKey('API_KEY_HERE');
+sgMailTorres.setApiKey('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');
 
-module.exports = sgMail;
+module.exports = sgMail, sgMailTorres;
