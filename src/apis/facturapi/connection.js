fd9697322ec5802c8936428ef77967f54ce20a03
Jaime Torres Reyes <jacatorresre@ittepic.edu.mx>
2024-11-15 20:26:05 -0700

    Agregando mi api_key Torres Reyes connection.js

--- src/apis/facturapi/connection.js
+++ src/apis/facturapi/connection.js
@@ -3,5 +3,6 @@ const Facturapi = require('facturapi').default;
 // b) Crea una instancia del cliente, usando la llave secreta
 //    de la organización emisora (https://dashboard.facturapi.io/integration/apikeys)
 const facturapi = new Facturapi("API_KEY_HERE");
+const facturapiTorres = new Facturapi(sk_test_v7gw215dbQVnYyBpXlbyL2kKkEjP9lALaeDRMNWqr6);
 
 module.exports = facturapi;
